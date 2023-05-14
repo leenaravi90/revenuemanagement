d3121628ff13e36e12cf0a4535deeca82a0436b9
leenaravi90 <133470211+leenaravi90@users.noreply.github.com>
2023-05-14 10:12:29 +0800

    Update sales.py

--- sales.py
+++ sales.py
@@ -31,4 +31,4 @@ loaded_model = pickle.load(open("sales.h5", "rb"))
 prediction = loaded_model.predict(df)
 
 st.subheader('Prediction')
-st.write (prediction[0].style.format('{:.2f}'))
+st.write(prediction[0].style.format('{:.2f}'))
